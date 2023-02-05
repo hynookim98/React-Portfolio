9ddbbab12f97edf95ddaaed7915f1890bc22e39d
hynookim98 <hynookim98@gmail.com>
2023-02-04 19:26:50 -0800

    added projects.js for portfolio

---
+++ src/components/Projects/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Project({ project }) {
+
+  const { name, repo, link, description } = project;
+
+  return (
+    <div className="project" key={name}>
+      <img
+        src={require(`../../assets/projects/${name}.jpg`).default}
+        alt={"project"}
+      />
+      <div>
+        <h3>
+          <a href={link}>Project Link</a>{' '}
+          <a href={repo}>
+            <i className="fab fa-github"></i>
+          </a>
+        </h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
